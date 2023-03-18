ef57216cf0c0d313cdb0432f03bb92795e575907
ttvnguyen <ttvnguyen@gmail.com>
2023-03-17 20:32:22 -0400

    frontend is done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  // const URL = "http://localhost:4000/";
+  const URL = "https://hw18-react-express.herokuapp.com/"
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/projects" element={<Projects URL={URL}/>}/>
+        <Route exact path="/about" element={<About URL={URL}/>}/>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+function Footer(props) {
+    return <h1>@Vi Nguyen 2023</h1>;
+  }
+  
+  export default Footer;
+  
